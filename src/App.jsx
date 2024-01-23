d9aebbe047a35f325efe875a8752963386ccce1a
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-23 21:11:17 +0200

    first commit

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import css from '';
-
-export const App = () => {
-  return <></>;
-};
---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import css from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={css.container}>
+      <h1>Serhii Leonov</h1>
+    </div>
+  );
+};
