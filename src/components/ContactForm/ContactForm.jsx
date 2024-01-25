753c98ef496f1c1a4ec537f8bccff4e3b95d8f75
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-25 14:35:52 +0200

    Contact Form validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,20 @@
 import { useId } from 'react';
-import { Formik, Form, Field } from 'formik';
-import css from './ContactForm.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js';
+
+import css from './ContactForm.module.css';
+
+const ContactFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too short!')
+    .max(50, 'Too long!')
+    .required('Required'),
+  number: Yup.string()
+    .min(3, 'Too short!')
+    .max(50, 'Too long')
+    .required('Required'),
+});
 
 const initialValues = { name: '', number: '' };
 
@@ -18,13 +31,18 @@ export const ContactForm = ({ onAdd }) => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSaubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSaubmit}
+      validationSchema={ContactFormSchema}
+    >
       <Form className={css.form}>
         <div>
           <label className={css.label} htmlFor={nameFieldId}>
             Name
           </label>
           <Field className={css.input} type="text" name="name" />
+          <ErrorMessage name="name" as="span" />
         </div>
 
         <div>
@@ -32,6 +50,7 @@ export const ContactForm = ({ onAdd }) => {
             Number
           </label>
           <Field className={css.input} type="tel" name="number" />
+          <ErrorMessage name="number" as="span" />
         </div>
 
         <button className={css.button} type="submit">
